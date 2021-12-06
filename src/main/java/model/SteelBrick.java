eef56a6f5941fa9c11dfae9a5cc70ab8bb3c8f77
Jason0w0 <jasonthesaber@gmail.com>
2021-12-07 01:52:44 +0800

    change steel brick from random hit to strength of 3

--- src/main/java/model/SteelBrick.java
+++ src/main/java/model/SteelBrick.java
@@ -19,24 +19,22 @@ package model;
 
 
 import java.awt.*;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
-import java.util.Random;
 
 
 public class SteelBrick extends Brick {
 
-//    private static final String NAME = "Steel Brick";
     private static final Color DEF_INNER = new Color(203, 203, 201);
     private static final Color DEF_BORDER = Color.BLACK;
-    private static final int STEEL_STRENGTH = 1;
-    private static final double STEEL_PROBABILITY = 0.4;
+    private static final int STEEL_STRENGTH = 3;
 
-    private final Random rnd;
-    private final Shape brickFace;
+    private final Crack crack;
+    private Shape brickFace;
 
     public SteelBrick(Point point, Dimension size){
         super(point,size,DEF_BORDER,DEF_INNER,STEEL_STRENGTH);
-        rnd = new Random();
+        crack = new Crack(DEF_CRACK_DEPTH,DEF_STEPS);
         brickFace = super.brickFace;
     }
 
@@ -51,17 +49,30 @@ public class SteelBrick extends Brick {
         return brickFace;
     }
 
-    public  boolean setImpact(Point2D point , int dir){
+    @Override
+    public boolean setImpact(Point2D point, int dir) {
         if(super.isBroken())
             return false;
-        impact();
-        return  super.isBroken();
+        super.impact();
+        if(!super.isBroken()){
+            crack.makeCrack(point,dir);
+            updateBrick();
+            return false;
+        }
+        return true;
     }
 
-    public void impact(){
-        if(rnd.nextDouble() < STEEL_PROBABILITY){
-            super.impact();
+    private void updateBrick(){
+        if(!super.isBroken()){
+            GeneralPath gp = crack.draw();
+            gp.append(super.brickFace,false);
+            brickFace = gp;
         }
     }
 
+    public void repair(){
+        super.repair();
+        crack.reset();
+        brickFace = super.brickFace;
+    }
 }
